59b22a8a5f59f78a26b8b4c2780b3e3b9b54feb6
Dan Yunin <DaniilUnin2002@yandex.ru>
2023-08-08 12:24:58 +0300

    ready solution for this task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    const resultArray = [];
+    // Если matrix не = undefined, то для неё можно вызывать метод
+    if (matrix !== undefined) {
+        matrix.forEach((row, index) => {
+            // Если index чётный, то просто копируем значения в том же порядке
+            // А в противном случае реверсируем значения и тоже копируем
+            if (index % 2 === 0) {
+                resultArray.push(...row);
+            } else {
+                resultArray.push(...row.reverse());
+            }
+        });
+    }
+    return resultArray;
 }
